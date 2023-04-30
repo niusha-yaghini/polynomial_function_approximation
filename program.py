040b6326b7f7d4bd76243a30e87fbb82d59bf75a
Niusha_ygn <niusha.yaghini@gmail.com>
2023-04-30 10:04:22 +0430

    compare tournoment and roulette wheel

--- program.py
+++ program.py
@@ -99,11 +99,64 @@ def Genetic(input_file_name):
     draw_average_mae(x_generation_number, y_average_mae_of_each, given_function)
 
 
+def Genetic_for_compare(input_file_name):
+    
+    f = open(f'{input_file_name}', 'r')
+    given_function = f.readline().split(':')[1]
+    X = []
+    Y = []
+    for i in range(amount):
+        a = f.readline().split(',')
+        X.append(float(a[0]))
+        Y.append(float(a[1]))
+        
+    # population number zero
+    print("population number 0")
+    list_of_parents = tree.all_trees(amount_of_trees, max_depth)
+    parents_average_mae, parents_best_mae, best_parent_tree = tree.calculating_mae(list_of_parents, X, Y)
+    
+    y_best_mae_of_each = []
+    
+    y_best_mae_of_each.append(parents_best_mae)
+    y_min_mae = min(y_best_mae_of_each)
+
+    for i in range(amount_of_generations):
+        
+        if(Termination_condition(y_min_mae)):
+            return
+    
+        print(f"population number {i+1}")
+        list_of_children = children.making_children(list_of_parents, k, pc, pm)
+        average_mae, best_mae, best_tree = tree.calculating_mae(list_of_children, X, Y)
+        list_of_parents = list_of_children
+        
+        y_best_mae_of_each.append(best_mae)
+        y_min_mae = min(y_best_mae_of_each)
+        
+    return y_min_mae
+
+
+def compare_tournoment_roulette(iteration, input_file_name):
+
+    f = open('compare_tournoment_roulettewheel.txt', 'a')
+    f.write("roulette wheel \n")
+    
+    tournoment = []
+    for i in range(iteration):
+        print(f"iteration {i}\n")
+        y_min = Genetic_for_compare(input_file_name)
+        tournoment.append(y_min)
+        f.write(f"best mae of iteration {i} is: {y_min}\n")
+    
+    avg = sum(tournoment)/len(tournoment)
+    f.write(f"average is: {avg}")
+    f.close()
+    
 if __name__ == "__main__":
     
-    rnd.seed(1)
+    # rnd.seed(1)
     
-    photo_number = 13
+    photo_number = 14
     
     # parameters
     amount = 100
@@ -120,7 +173,9 @@ if __name__ == "__main__":
     
     input_file_name = 'in_out2.txt'
     
-    Genetic(input_file_name)
+    # Genetic(input_file_name)
+    iteration = 40
+    compare_tournoment_roulette(iteration, input_file_name)
 
     print()
     
